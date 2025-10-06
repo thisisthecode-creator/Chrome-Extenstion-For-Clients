2814d424dd5cc740a60513edfea76f725e8cf454
Florian <florian@mac-2.home>
2025-10-07 00:55:30 +0200

    Make Benefit Systems logo a clickable link
    - Wrapped logo image in anchor tag linking to https://www.benefitsystems.io
    - Added target='_blank' to open link in new tab
    - Added CSS styles for logo link with hover effect (opacity transition)
    - Logo now functions as a clickable link to the main Benefit Systems website

--- content.js
+++ content.js
@@ -40,7 +40,9 @@ function injectExtensionPanel() {
   panel.innerHTML = `
     <!-- Benefit Systems Logo -->
     <div class="bs-logo-container">
-      <img src="https://saegzrncsjcsvgcjkniv.supabase.co/storage/v1/object/sign/Logo/BenefitSystems.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV83ZWM2ZTk3Zi03YjQ2LTQ0ODMtODNjMS00ZDQwODU5N2MyOTgiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJMb2dvL0JlbmVmaXRTeXN0ZW1zLnBuZyIsImlhdCI6MTc1OTc5MDg1MSwiZXhwIjoxNzY4NDMwODUxfQ.wRo7pPjUbmM-Rzo0fXGUChNcgwpM_aTnMjlz4IB5RHk" alt="Benefit Systems" class="bs-logo" />
+      <a href="https://www.benefitsystems.io" target="_blank" class="bs-logo-link">
+        <img src="https://saegzrncsjcsvgcjkniv.supabase.co/storage/v1/object/sign/Logo/BenefitSystems.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV83ZWM2ZTk3Zi03YjQ2LTQ0ODMtODNjMS00ZDQwODU5N2MyOTgiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJMb2dvL0JlbmVmaXRTeXN0ZW1zLnBuZyIsImlhdCI6MTc1OTc5MDg1MSwiZXhwIjoxNzY4NDMwODUxfQ.wRo7pPjUbmM-Rzo0fXGUChNcgwpM_aTnMjlz4IB5RHk" alt="Benefit Systems" class="bs-logo" />
+      </a>
     </div>
     
     <div class="bs-content" id="bs-collapsible-content">
